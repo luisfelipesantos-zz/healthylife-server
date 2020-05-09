2dc9664c62b6ac5e98dbf3d702210c644dca554a
Luis Felipe Santos <luisfelipesantosdev@gmail.com>
2020-05-09 01:32:11 +0000

    changing database location

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
-    "username": "root",
-    "password": null,
+    "username": "luis",
+    "password": "",
     "database": "healthylife",
     "host": "localhost",
     "dialect": "mysql",
--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ const app = express();
 
 app.use(express.json());
 
+app.get('/', (req, res) => {
+	return res.send('Hello World!'); 
+});
+
 app.use(routes);
 
+console.log('listening on port 8000...');
 app.listen(8000);
