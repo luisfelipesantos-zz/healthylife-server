534b8183026269443f78b65b584334aef65f75e2
Luis Felipe Santos <luisfelipetrompete@gmail.com>
2020-05-21 01:09:20 +0000

    environment variables

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,10 @@
+require('dotenv/config');
+
 module.exports = {
-    "username": "luis",
-    "password": "",
-    "database": "healthylife",
-    "host": "localhost",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DBNAME,
+    "host": process.env.DB_HOST,
     "dialect": "mysql",
     define: {
         timestamps: false,
